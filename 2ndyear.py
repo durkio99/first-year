1efe1b604bf3faa01e6ad418f363bd30ac354367
durkio99 <163888487+durkio99@users.noreply.github.com>
2024-06-21 10:36:30 +0000

    created java and python file

---
+++ 2ndyear.py
@@ -0,0 +1,8 @@
+num1 = 444
+num2 = 222
+print (num1+num2)
+
+num1 = 45
+num2 = 25
+print (num1-num2)
+
---
+++ java1.java
@@ -0,0 +1,13 @@
+class test {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 20;
+        if (a > b) {
+            System.out.println("a is maximum");
+
+        } else {
+            System.out.println("b is maximum");
+        }
+    }
+
+}
